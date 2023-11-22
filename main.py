c36a948a68295e986b1585de1e75675917b0e796
Harry Raj <113687752+harry10-git@users.noreply.github.com>
2023-11-22 10:02:54 +0530

    updated test size of 0.3

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ for key, val in encode.items():
     decode[val] = key
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 dt_model = DecisionTreeClassifier(random_state=42)
 
